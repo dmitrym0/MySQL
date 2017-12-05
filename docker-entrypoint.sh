498937c996ca3a367be52f01fe9190f1c05c7fc0
asamo7 <asamo7@gmail.com>
2017-12-05 15:01:16 +0100

    Solucionada incidència amb creació d'usuari MYSQL_USER

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -121,10 +121,9 @@ EOF
 		fi
 
 		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
-			echo "CREATE USER '"$MYSQL_USER"'@'%' IDENTIFIED BY '"$MYSQL_PASSWORD"' ;" | "${mysql[@]}"
-
+			echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" | "${mysql[@]}"
 			if [ "$MYSQL_DATABASE" ]; then
-				echo "GRANT ALL ON \`"$MYSQL_DATABASE"\`.* TO '"$MYSQL_USER"'@'%' ;" | "${mysql[@]}"
+				echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" | "${mysql[@]}"
 			fi
 
 			echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
--- fix-permissions.sh
+++ fix-permissions.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-MYSQL_USER=mysql
+MYSQL_OS_USER=mysql
 
 [[ "$UID" ]] || UID=$(id -u)
 
 if [ $UID -eq 0 ] ; then
 
 	chmod a+rw /var/log/mysqld.log
-	chown -R $MYSQL_USER: /var/run/mysqld/
+	chown -R $MYSQL_OS_USER: /var/run/mysqld/
 	
 fi
 
